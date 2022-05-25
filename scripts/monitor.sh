fc6529352647e6d261ca5dd09305d79935428a68
tqxia <txiadev@gmail.com>
2022-05-25 13:56:31 +0800

    Add median CPU usage and update wording

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -2,18 +2,20 @@
 
 pid=$1
 duration=$2
-echo "start sampling, pid: $pid, duration: $duration seconds"
+echo "Start sampling, pid: $pid, duration: $duration seconds"
 
 top -b -d 1 -p $pid > tmp.log &
 tpid=$!
-echo "sampling process(top) pid: $tpid"
+echo "Sampling process(top) pid: $tpid"
 
 sleep $duration
 kill -9 $tpid
-# process data
+# process & display data
+echo "CPU usage statistics:"
 grep $pid tmp.log > filtered.tmp.log
-cat filtered.tmp.log | awk '{sum+=$9} END {print "Average cpu usage =", sum/NR}'
-cat filtered.tmp.log | awk 'BEGIN {max = 0} {if ($9+0 > max+0) max=$9 fi} END {print "Max cpu usage =", max}'
-cat filtered.tmp.log | awk 'BEGIN {min = 65536} {if ($9+0 < min+0) min=$9 fi} END {print "Min cpu usage =", min}'
+cat filtered.tmp.log | awk '{sum+=$9} END {print "Average =", sum/NR}'
+cat filtered.tmp.log | awk '{print $9}' | sort | awk ' {a[i++] = $1} END {print "Median =", a[int(i/2)]}'
+cat filtered.tmp.log | awk 'BEGIN {max = 0} {if ($9+0 > max+0) max=$9 fi} END {print "Max =", max}'
+cat filtered.tmp.log | awk 'BEGIN {min = 65536} {if ($9+0 < min+0) min=$9 fi} END {print "Min cpu =", min}'
 # cleanup
 rm -f *tmp.log
