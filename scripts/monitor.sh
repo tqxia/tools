953c08f3d3591464ba275104924cc6a89f303e67
tqxia <txiadev@gmail.com>
2022-05-25 12:06:13 +0800

    Fix average cpu usage calculation

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -2,7 +2,7 @@
 
 pid=$1
 duration=$2
-echo "start sampling, pid: $pid, duration: $duration"
+echo "start sampling, pid: $pid, duration: $duration seconds"
 
 top -b -d 1 -p $pid > tmp.log &
 tpid=$!
@@ -12,7 +12,7 @@ sleep $duration
 kill -9 $tpid
 # process data
 grep $pid tmp.log > filtered.tmp.log
-cat filtered.tmp.log | awk '{sum+=$1} END {print "Average cpu usage = ", sum/NR}'
+cat filtered.tmp.log | awk '{sum+=$9} END {print "Average cpu usage =", sum/NR}'
 cat filtered.tmp.log | awk 'BEGIN {max = 0} {if ($9+0 > max+0) max=$9 fi} END {print "Max cpu usage =", max}'
 cat filtered.tmp.log | awk 'BEGIN {min = 65536} {if ($9+0 < min+0) min=$9 fi} END {print "Min cpu usage =", min}'
 # cleanup
