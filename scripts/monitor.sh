93d854f459ab2c951e6e70cd343735fba1507608
tqxia <txiadev@gmail.com>
2022-05-25 14:04:21 +0800

    Fix wording, disable kill output

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -2,16 +2,16 @@
 
 pid=$1
 duration=$2
-echo "Start sampling, pid: $pid, duration: $duration seconds"
+echo "Start sampling, target pid: $pid, duration: $duration seconds"
 
 top -b -d 1 -p $pid > tmp.log &
 tpid=$!
 echo "Sampling process(top) pid: $tpid"
 
 sleep $duration
-kill -9 $tpid
+kill $tpid
 # process & display data
-echo "CPU usage statistics:"
+printf "\nCPU usage statistics:\n"
 grep $pid tmp.log > filtered.tmp.log
 cat filtered.tmp.log | awk '{sum+=$9} END {print "Average =", sum/NR}'
 cat filtered.tmp.log | awk '{print $9}' | sort | awk ' {a[i++] = $1} END {print "Median =", a[int(i/2)]}'
